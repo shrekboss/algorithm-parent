50254316aef7b89ceeb268bfb1f449bb97a71de7
yizhe.chen <cyz_shrek2012@foxmail.com>
2019-11-22 11:06:35 +0800

    调整

--- src/org/crayzer/algorithm/array/LRUBasedArray.java
+++ src/org/crayzer/algorithm/array/LRUBasedArray.java
@@ -32,6 +32,10 @@ public class LRUBasedArray<T> {
         holder = new HashMap<>(capacity);
     }
 
+    /**
+     * 模拟访问某个值
+     * @param object
+     */
     public void offer(T object) {
         if (null == object) {
             throw new IllegalArgumentException("缓存容器不支持null!");
@@ -50,6 +54,10 @@ public class LRUBasedArray<T> {
         }
     }
 
+    /**
+     * 若缓存中有指定的值，则更新位置
+     * @param end
+     */
     public void update(int end) {
         T target = value[end];
         rightShift(end);
@@ -57,19 +65,32 @@ public class LRUBasedArray<T> {
         holder.put(target, 0);
     }
 
-    public void cache(T target, int end) {
+    /**
+     * 缓存数据到头部，但要先右移
+     * @param object
+     * @param end 数组右移的边界
+     */
+    public void cache(T object, int end) {
         rightShift(end);
-        value[0] = target;
-        holder.put(target, 0);
+        value[0] = object;
+        holder.put(object, 0);
         count++;
     }
 
+    /**
+     * 缓存满的情况，踢出后，再缓存到数组头部
+     * @param object
+     */
     public void removeAndCache(T object) {
         T key = value[--count];
         holder.remove(key);
         cache(object, count);
     }
 
+    /**
+     * end左边的数据统一右移一位
+     * @param end
+     */
     private void rightShift(int end) {
         for (int i = end - 1; i >= 0; i--) {
             value[i + 1] = value[i];
@@ -91,60 +112,57 @@ public class LRUBasedArray<T> {
         return sb.toString();
     }
 
-    static class TestLRUBasedArray {
+    public static void main(String[] args) {
+        testDefaultConstructor();
+        testSpecifiedConstructor(4);
+        // testWithException();
+    }
 
-        public static void main(String[] args) {
-            // testDefaultConstructor();
-            // testSpecifiedConstructor(4);
-            testWithException();
-        }
+    private static void testWithException() {
+        LRUBasedArray<Integer> lru = new LRUBasedArray<Integer>();
+        lru.offer(1);
+        lru.offer(null);
+        lru.offer(2);
+        lru.offer(null);
+        System.out.println(lru);
+    }
 
-        private static void testWithException() {
-            LRUBasedArray<Integer> lru = new LRUBasedArray<Integer>();
-            lru.offer(1);
-            lru.offer(null);
-            lru.offer(2);
-            lru.offer(null);
-            System.out.println(lru);
-        }
+    public static void testDefaultConstructor() {
+        System.out.println("======无参测试========");
+        LRUBasedArray<Integer> lru = new LRUBasedArray<Integer>();
+        lru.offer(1);
+        lru.offer(2);
+        lru.offer(3);
+        lru.offer(4);
+        lru.offer(5);
+        System.out.println(lru);
+        lru.offer(6);
+        lru.offer(7);
+        lru.offer(8);
+        lru.offer(9);
+        System.out.println(lru);
+    }
 
-        public static void testDefaultConstructor() {
-            System.out.println("======无参测试========");
-            LRUBasedArray<Integer> lru = new LRUBasedArray<Integer>();
-            lru.offer(1);
-            lru.offer(2);
-            lru.offer(3);
-            lru.offer(4);
-            lru.offer(5);
-            System.out.println(lru);
-            lru.offer(6);
-            lru.offer(7);
-            lru.offer(8);
-            lru.offer(9);
-            System.out.println(lru);
-        }
-
-        public static void testSpecifiedConstructor(int capacity) {
-            System.out.println("======有参测试========");
-            LRUBasedArray<Integer> lru = new LRUBasedArray<Integer>(capacity);
-            lru.offer(1);
-            System.out.println(lru);
-            lru.offer(2);
-            System.out.println(lru);
-            lru.offer(3);
-            System.out.println(lru);
-            lru.offer(4);
-            System.out.println(lru);
-            lru.offer(2);
-            System.out.println(lru);
-            lru.offer(4);
-            System.out.println(lru);
-            lru.offer(7);
-            System.out.println(lru);
-            lru.offer(1);
-            System.out.println(lru);
-            lru.offer(2);
-            System.out.println(lru);
-        }
+    public static void testSpecifiedConstructor(int capacity) {
+        System.out.println("======有参测试========");
+        LRUBasedArray<Integer> lru = new LRUBasedArray<Integer>(capacity);
+        lru.offer(1);
+        System.out.println(lru);
+        lru.offer(2);
+        System.out.println(lru);
+        lru.offer(3);
+        System.out.println(lru);
+        lru.offer(4);
+        System.out.println(lru);
+        lru.offer(2);
+        System.out.println(lru);
+        lru.offer(4);
+        System.out.println(lru);
+        lru.offer(7);
+        System.out.println(lru);
+        lru.offer(1);
+        System.out.println(lru);
+        lru.offer(2);
+        System.out.println(lru);
     }
 }
