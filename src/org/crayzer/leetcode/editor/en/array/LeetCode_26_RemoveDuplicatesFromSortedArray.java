a7e456601435b57d1eda2313f83bb4c5226bd591
yizhe.chen <cyz_shrek2012@foxmail.com>
2019-12-16 00:45:06 +0800

    [+] code review@!

--- src/org/crayzer/leetcode/editor/en/array/LeetCode_26_RemoveDuplicatesFromSortedArray.java
+++ src/org/crayzer/leetcode/editor/en/array/LeetCode_26_RemoveDuplicatesFromSortedArray.java
@@ -50,7 +50,7 @@ public class LeetCode_26_RemoveDuplicatesFromSortedArray {
     //         int i = 0;
     //         for (int j = 1; j < nums.length; j++) {
     //             if (nums[i] != nums[j]) {
-    //                 nums[i++] = nums[j];
+    //                 nums[++i] = nums[j];
     //             }
     //         }
     //         return i + 1;
