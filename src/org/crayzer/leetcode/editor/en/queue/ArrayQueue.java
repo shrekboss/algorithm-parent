a22772a3cb326b3769a674ce766bd7eb82a48ebb
yizhe.chen <cyz_shrek2012@foxmail.com>
2019-12-16 17:07:56 +0800

    [+] code review@!

--- src/org/crayzer/leetcode/editor/en/queue/ArrayQueue.java
+++ src/org/crayzer/leetcode/editor/en/queue/ArrayQueue.java
@@ -1,37 +1,28 @@
 package org.crayzer.leetcode.editor.en.queue;
 
-
-// 用数组实现的队列
 public class ArrayQueue {
+
     private String[] items;
-    private int size = 0;
-    // head表示队头下标，tail表示队尾下标
+    private int n = 0;
     private int head = 0;
     private int tail = 0;
 
-    // 申请一个大小为capacity的数组
     public ArrayQueue(int capacity) {
         items = new String[capacity];
-        size = capacity;
+        this.n = capacity;
     }
 
-    // 入队
     public boolean enqueue(String item) {
-        // 如果tail == size 表示队列已经满了
-        if (tail == size) return false;
-        items[tail] = item;
-        ++tail;
+        if (tail == n) return false;
+
+        items[tail++] = item;
         return true;
     }
 
-    // 出队
     public String dequeue() {
-        // 如果head == tail 表示队列为空
         if (head == tail) return null;
-        // 为了让其他语言的同学看的更加明确，把--操作放到单独一行来写了
-        String ret = items[head];
-        ++head;
-        return ret;
+
+        return items[head++];
     }
 
     public static void main(String[] args) {
@@ -41,6 +32,8 @@ public class ArrayQueue {
         arrayQueue.enqueue("3");
 
         arrayQueue.dequeue();
+        String ret = arrayQueue.dequeue();
+        System.out.println(ret);
 
         for (int i = arrayQueue.head; i < arrayQueue.tail; i++) {
             System.out.print(arrayQueue.items[i] + " ");
