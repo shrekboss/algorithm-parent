e290589cab7d737d21c12f916cde7972c3b86f3a
yizhe.chen <cyz_shrek2012@foxmail.com>
2019-12-16 18:02:49 +0800

    [+] 基于链表实现简单队列

---
+++ src/org/crayzer/leetcode/editor/en/queue/CircularQueue.java
@@ -0,0 +1,34 @@
+package org.crayzer.leetcode.editor.en.queue;
+
+public class CircularQueue {
+
+    private String[] items;
+    private int n;
+    private int head;
+    private int tail;
+
+    public CircularQueue(int capacity) {
+        items = new String[capacity];
+        this.n = capacity;
+    }
+
+    public boolean enqueue(String item) {
+        /** 队列满了 */
+        if ((tail + 1) % n == head) return false;
+
+        items[tail] = item;
+        tail = (tail + 1) % n;
+
+        return true;
+    }
+
+    public String dequeue() {
+        /** 如果head == tail 表示队列为空 */
+        if (head == tail) return null;
+
+        String rtn = items[head];
+        head = (head + 1) % n;
+
+        return rtn;
+    }
+}
