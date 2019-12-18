7afde91295b1cfba32664985220d076d7b94b5c2
yizhe.chen <cyz_shrek2012@foxmail.com>
2019-12-18 11:42:32 +0800

    [+] 二叉树前序遍历 解法提交@！

---
+++ src/org/crayzer/leetcode/editor/en/tree/LeetCode_144_BinaryTreePreorderTraversal.java
@@ -0,0 +1,86 @@
+package org.crayzer.leetcode.editor.en.tree;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class LeetCode_144_BinaryTreePreorderTraversal {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * 递归
+     */
+    class Solution3 {
+        public List<Integer> preorderTracersal(TreeNode root) {
+            List<Integer> res = new LinkedList<>();
+            retrieves(root, res);
+            return res;
+        }
+
+        private void retrieves(TreeNode root, List<Integer> res) {
+            if (root == null) return;
+
+            res.add(root.val);
+            retrieves(root.left, res);
+            retrieves(root.right, res);
+        }
+    }
+
+    /**
+     * 基于链表实现
+     */
+    class Solution2 {
+
+        public List<Integer> preorderTracersal(TreeNode root) {
+            LinkedList<Integer> result = new LinkedList<>();
+            LinkedList<TreeNode> stack = new LinkedList<>();
+
+            if (root == null) return result;
+
+            stack.add(root);
+            while (!stack.isEmpty()) {
+                TreeNode treeNode = stack.pollLast();
+                result.add(treeNode.val);
+
+                /* 注意：顺序 */
+                if (treeNode.right != null) stack.add(treeNode.right);
+                if (treeNode.left != null) stack.add(treeNode.left);
+            }
+
+            return result;
+        }
+    }
+
+    /**
+     * 基于栈实现
+     */
+    class Solution1 {
+        public List<Integer> preorderTraversal(TreeNode root) {
+            List<Integer> result = new LinkedList<>();
+            Stack<TreeNode> stack = new Stack<>();
+
+            if (root == null) return result;
+
+            stack.add(root);
+            while (!stack.isEmpty()) {
+                TreeNode treeNode = stack.pop();
+                result.add(treeNode.val);
+
+                /* 栈是先入后出 */
+                if (treeNode.right != null) stack.push(treeNode.right);
+                if (treeNode.left != null) stack.push(treeNode.left);
+            }
+
+            return result;
+        }
+    }
+}
