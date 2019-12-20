9862e2ce9afcf15fbe97a73ceb01a97c0e95986a
yizhe.chen <cyz_shrek2012@foxmail.com>
2019-12-20 13:27:32 +0800

    [+] 4Sum 重写@！

--- src/org/crayzer/leetcode/editor/en/array/LeetCode_18_4Sum.java
+++ src/org/crayzer/leetcode/editor/en/array/LeetCode_18_4Sum.java
@@ -1,6 +1,5 @@
 package org.crayzer.leetcode.editor.en.array;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -9,11 +8,12 @@ public class LeetCode_18_4Sum {
 
     class Solution {
         public List<List<Integer>> fourSum(int[] nums, int target) {
-            List<List<Integer>> res = new ArrayList<>();
+            List<List<Integer>> res = new LinkedList<>();
             if (nums == null || nums.length < 4) return res;
 
             Arrays.sort(nums);
             int length = nums.length;
+
             for (int k = 0; k < length - 3; k++) {
                 if (k > 0 && nums[k] == nums[k - 1]) continue;
 
@@ -38,18 +38,17 @@ public class LeetCode_18_4Sum {
                     while (j < h) {
                         int curr = nums[k] + nums[i] + nums[j] + nums[h];
                         if (curr < target) {
-                            while (j < h && nums[j] == nums[++j]) ;
+                            while (j < h && nums[j] == nums[++j]);
                         } else if (curr > target) {
-                            while (j < h && nums[h] == nums[--h]) ;
+                            while (j < h && nums[h] == nums[--h]);
                         } else {
                             res.add(new LinkedList<>(Arrays.asList(nums[k], nums[i], nums[j], nums[h])));
-                            while (j < h && nums[j] == nums[j++]) ;
-                            while (j < h && nums[h] == nums[--h]) ;
+                            while (j < h && nums[j] == nums[++j]);
+                            while (j < h && nums[h] == nums[--h]);
                         }
                     }
                 }
             }
-
             return res;
         }
     }
