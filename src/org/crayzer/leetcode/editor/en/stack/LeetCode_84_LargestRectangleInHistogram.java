e2d37f93c85736ee3d127234f153773c86694fc2
yizhe.chen <cyz_shrek2012@foxmail.com>
2019-12-25 00:22:06 +0800

    [+]  柱状图中最大的矩形 解法提交@！

--- src/org/crayzer/leetcode/editor/en/stack/LeetCode_84_LargestRectangleInHistogram.java
+++ src/org/crayzer/leetcode/editor/en/stack/LeetCode_84_LargestRectangleInHistogram.java
@@ -1,5 +1,7 @@
 package org.crayzer.leetcode.editor.en.stack;
 
+import java.util.Stack;
+
 public class LeetCode_84_LargestRectangleInHistogram {
 
     /**
@@ -7,7 +9,20 @@ public class LeetCode_84_LargestRectangleInHistogram {
      */
     class Solution3 {
         public int largestRectangleArea(int[] heights) {
-            return 0;
+            Stack<Integer> stack = new Stack<>();
+            stack.push(-1);
+
+            int maxArea = 0;
+            for (int i = 0; i < heights.length; i++) {
+                while (stack.peek() != -1 && heights[stack.peek()] >= heights[i])
+                    maxArea = Math.max(maxArea, heights[stack.pop()] * (i - stack.peek() - 1));
+                stack.push(i);
+            }
+
+            while (stack.peek() != -1) {
+                maxArea = Math.max(maxArea, heights[stack.pop()] * (heights.length - stack.peek()- 1));
+            }
+            return maxArea;
         }
     }
 
