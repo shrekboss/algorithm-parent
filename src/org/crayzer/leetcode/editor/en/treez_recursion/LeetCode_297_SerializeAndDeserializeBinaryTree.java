45397a8ed1f0372e206335927aa1112483081908
yizhe.chen <cyz_shrek2012@foxmail.com>
2019-12-26 11:45:16 +0800

    [+]  二叉树的序列化与反序列化 重写@！

--- src/org/crayzer/leetcode/editor/en/treez_recursion/LeetCode_297_SerializeAndDeserializeBinaryTree.java
+++ src/org/crayzer/leetcode/editor/en/treez_recursion/LeetCode_297_SerializeAndDeserializeBinaryTree.java
@@ -2,27 +2,24 @@ package org.crayzer.leetcode.editor.en.treez_recursion;
 
 import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class LeetCode_297_SerializeAndDeserializeBinaryTree {
 
     class Solution {
-
         private final static String SEPARATOR = ",";
-        private final static String NN = "#";
+        private final static String NULL = "#";
 
         public String serialize(TreeNode root) {
             return doSerialize(root, new StringBuilder()).toString();
         }
 
         private StringBuilder doSerialize(TreeNode root, StringBuilder builder) {
-            if (root == null) builder.append(NN).append(SEPARATOR);
+            if (root == null) builder.append(NULL).append(SEPARATOR);
             else {
                 builder.append(root.val).append(SEPARATOR);
                 doSerialize(root.left, builder);
                 doSerialize(root.right, builder);
             }
-
             return builder;
         }
 
@@ -30,11 +27,11 @@ public class LeetCode_297_SerializeAndDeserializeBinaryTree {
             return doDeserialize(new LinkedList<>(Arrays.asList(data.split(SEPARATOR))));
         }
 
-        private TreeNode doDeserialize(Queue<String> dataList) {
-            String root = dataList.remove();
-            if (root.equals(NN)) return null;
+        private TreeNode doDeserialize(LinkedList<String> dataList) {
+            String val = dataList.remove();
+            if (val.equals(NULL)) return null;
             else {
-                TreeNode node = new TreeNode(Integer.parseInt(root));
+                TreeNode node = new TreeNode(Integer.parseInt(val));
                 node.left = doDeserialize(dataList);
                 node.right = doDeserialize(dataList);
 
