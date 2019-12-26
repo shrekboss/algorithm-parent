6eb6ab9806084f0d1472f7ad2ba2825b599b1724
yizhe.chen <cyz_shrek2012@foxmail.com>
2019-12-26 14:23:36 +0800

    [+]  二叉树的最小深度 解法提交@！

---
+++ src/org/crayzer/leetcode/editor/en/treez_recursion/LeetCode_111_MinimumDepthOfBinaryTree.java
@@ -0,0 +1,38 @@
+package org.crayzer.leetcode.editor.en.treez_recursion;
+
+public class LeetCode_111_MinimumDepthOfBinaryTree {
+
+    class Solution1 {
+        public int minDepth(TreeNode root) {
+            if (root == null) return 0;
+            if (root.left == null && root.right == null) return 1;
+            int m1 = minDepth(root.left);
+            int m2 = minDepth(root.right);
+            return (root.left == null || root.right == null) ? m1 + m2 + 1 : Math.min(m1, m2) + 1;
+        }
+    }
+
+    class Solution {
+        public int minDepth(TreeNode root) {
+            if (root == null) return 0;
+            if (root.left == null && root.right == null) return 1;
+
+            int m1 = minDepth(root.left);
+            int m2 = minDepth(root.right);
+
+            if (root.left == null || root.right == null) return m1 + m2 + 1;
+
+            return Math.min(m1, m2) + 1;
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
