8338596259c77c35905f12aa0a2c7750afecb53f
yizhe.chen <cyz_shrek2012@foxmail.com>
2019-12-30 11:25:38 +0800

    [+]  二分查找 解法提交@！

---
+++ src/org/crayzer/leetcode/editor/en/二分查找/LeetCode_704_BinarySearch.java
@@ -0,0 +1,19 @@
+package org.crayzer.leetcode.editor.en.二分查找;
+
+public class LeetCode_704_BinarySearch {
+
+    class Solution {
+        public int search(int[] nums, int target) {
+            if (nums == null) return -1;
+
+            int left = 0, right = nums.length - 1;
+            while (left < right) {
+                int mid = (left + right) >>> 1;
+                if (nums[mid] < target) left = mid + 1;
+                else right = mid;
+            }
+
+            return nums[left] == target ? left : -1;
+        }
+    }
+}
