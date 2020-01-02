1c6f478080fc41541a2509ca1325390867309770
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-01-02 10:33:57 +0800

    [+]  单词接龙 解法提交@！

--- src/org/crayzer/leetcode/editor/en/dfs_bfs/LeetCode_127_WordLadder.java
+++ src/org/crayzer/leetcode/editor/en/dfs_bfs/LeetCode_127_WordLadder.java
@@ -9,31 +9,30 @@ public class LeetCode_127_WordLadder {
     public static void main(String[] args) {
         String beginWord = "hit";
         String endWord = "cog";
-        String[] wordList = {"hot","dot","dog","lot","log","cog"};
+        String[] wordList = {"hot", "dot", "dog", "lot", "log", "cog"};
         Solution solution = new Solution();
         System.out.println(solution.ladderLength(beginWord, endWord, Arrays.asList(wordList)));
     }
 
     static class Solution {
-        private HashMap<String, LinkedList<String>> allComboDict;
         private int len;
+        private HashMap<String, LinkedList<String>> allComboDict;
 
         public Solution() {
-            len = 0;
+            this.len = 0;
             this.allComboDict = new HashMap<>();
         }
 
         public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-            if (!wordList.contains(endWord)) return 0;
-            len = beginWord.length();
+            this.len = beginWord.length();
 
             wordList.forEach(
                     word -> {
                         for (int i = 0; i < len; i++) {
                             String newWord = word.substring(0, i) + "*" + word.substring(i + 1, len);
-                            LinkedList<String> transformations = allComboDict.getOrDefault(newWord, new LinkedList<>());
-                            transformations.add(word);
-                            allComboDict.put(newWord, transformations);
+                            LinkedList<String> transformationns = this.allComboDict.getOrDefault(newWord, new LinkedList());
+                            transformationns.add(word);
+                            this.allComboDict.put(newWord, transformationns);
                         }
                     }
             );
@@ -55,22 +54,19 @@ public class LeetCode_127_WordLadder {
                 ans = visitedWordNode(endQueue, endVisited, beginVisited);
                 if (ans > -1) return ans;
             }
+
             return 0;
         }
 
-        private int visitedWordNode(Queue<Pair<String, Integer>> queue,
-                                    HashMap<String, Integer> visited,
-                                    HashMap<String, Integer> othersVisited) {
+        private int visitedWordNode(Queue<Pair<String, Integer>> queue, HashMap<String, Integer> visited, HashMap<String, Integer> otherVisited) {
             Pair<String, Integer> node = queue.remove();
             String word = node.getKey();
             int level = node.getValue();
 
             for (int i = 0; i < len; i++) {
                 String newWord = word.substring(0, i) + "*" + word.substring(i + 1, len);
-                for (String adjacentWord : allComboDict.getOrDefault(newWord, new LinkedList<>())) {
-                    if (othersVisited.containsKey(adjacentWord)) {
-                        return level + othersVisited.get(adjacentWord);
-                    }
+                for (String adjacentWord : this.allComboDict.getOrDefault(newWord, new LinkedList<>())) {
+                    if (otherVisited.containsKey(adjacentWord)) return level + otherVisited.get(adjacentWord);
                     if (!visited.containsKey(adjacentWord)) {
                         visited.put(adjacentWord, level + 1);
                         queue.add(new Pair<>(adjacentWord, level + 1));
@@ -79,9 +75,10 @@ public class LeetCode_127_WordLadder {
             }
             return -1;
         }
+
     }
 
-    class Solution1{
+    class Solution1 {
         public int ladderLength(String beginWord, String endWord, List<String> wordList) {
             int len = beginWord.length();
             HashMap<String, LinkedList<String>> allComboDict = new HashMap<>();
