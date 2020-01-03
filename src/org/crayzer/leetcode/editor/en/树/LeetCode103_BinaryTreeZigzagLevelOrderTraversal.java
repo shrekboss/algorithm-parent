b0ebb6ea261c5baa5676eb8935cb064a9515cc59
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-01-03 18:10:12 +0800

    [+]  二叉树的锯齿形层次遍历 解法提交@！

---
+++ src/org/crayzer/leetcode/editor/en/树/LeetCode103_BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,64 @@
+package org.crayzer.leetcode.editor.en.树;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LeetCode103_BinaryTreeZigzagLevelOrderTraversal {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    class Solution2 {
+        private List<List<Integer>> res = new LinkedList<>();
+
+        public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+            if (root == null) return res;
+            dfs(root, 0);
+            return res;
+        }
+
+        private void dfs(TreeNode root, int depth) {
+            if (depth >= res.size()) res.add(new LinkedList<>());
+
+            if (depth % 2 == 0) res.get(depth).add(root.val);
+            else res.get(depth).add(0, root.val);
+
+            if (root.left != null) dfs(root.left, depth + 1);
+            if (root.right != null) dfs(root.right, depth + 1);
+        }
+    }
+
+    class Solution1 {
+        public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+            List<List<Integer>> res = new LinkedList<>();
+            if (root == null) return res;
+
+            Queue<TreeNode> track = new LinkedList<>();
+            track.offer(root);
+
+            while (!track.isEmpty()) {
+                int size = track.size();
+                List<Integer> level = new LinkedList<>();
+
+                for (int i = 0; i < size; i++) {
+                    TreeNode cur = track.poll();
+                    if (res.size() % 2 == 0) level.add(cur.val);
+                    else level.add(0, cur.val);
+
+                    if (cur.left != null) track.offer(cur.left);
+                    if (cur.right != null) track.offer(cur.right);
+                }
+                res.add(new LinkedList<>(level));
+            }
+            return res;
+        }
+    }
+}
