c969fd6e8812e6739b7d543990c97b3a56f3cb40
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-01-03 10:48:58 +0800

    [+]  跳跃游戏 II 解法提交@！

--- src/org/crayzer/leetcode/editor/en/greedy/LeetCode_45_JumpGame2.java
+++ src/org/crayzer/leetcode/editor/en/greedy/LeetCode_45_JumpGame2.java
@@ -13,6 +13,12 @@ public class LeetCode_45_JumpGame2 {
             int maxPosition = 0;
             int steps = 0;
 
+            /*
+             * 这里要注意一个细节，就是 for 循环中，i < nums.length - 1，少了末尾。
+             * 因为开始的时候边界是第 00 个位置，steps 已经加 11 了。如下图，
+             * 如果最后一步刚好跳到了末尾，此时 steps 其实不用加 11 了。
+             * 如果是 i < nums.length，i 遍历到最后的时候，会进入 if 语句中，steps 会多加 11。
+             */
             for (int i = 0; i < nums.length - 1; i++) {
                 maxPosition = Math.max(maxPosition, nums[i] + i);
                 // 遇到边界，就更新边界，并且步数加一
@@ -21,7 +27,6 @@ public class LeetCode_45_JumpGame2 {
                     steps++;
                 }
             }
-
             return steps;
         }
     }
