aef1ea190da69ace154417f3b0ad02806c39faef
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-01-03 16:06:08 +0800

    [+]  合并两个有序链表 解法提交@！

--- src/org/crayzer/leetcode/editor/en/list/LeetCode_21_MergeTwoSortedLists.java
+++ src/org/crayzer/leetcode/editor/en/list/LeetCode_21_MergeTwoSortedLists.java
@@ -1,7 +1,7 @@
 package org.crayzer.leetcode.editor.en.list;
 
 public class LeetCode_21_MergeTwoSortedLists {
-     class ListNode {
+    class ListNode {
         int val;
         ListNode next;
 
@@ -10,25 +10,40 @@ public class LeetCode_21_MergeTwoSortedLists {
         }
     }
 
-    public ListNode mergeTwoList(ListNode l1, ListNode l2) {
-        ListNode soldier = new ListNode(0);
-        ListNode p = soldier;
-
-        while (l1 != null && l2 != null) {
+    class Solution {
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            if (l1 == null) return l2;
+            if (l2 == null) return l1;
             if (l1.val < l2.val) {
-                p.next = l1;
-                l1 = l1.next;
+                l1.next = mergeTwoLists(l1.next, l2);
+                return l1;
             } else {
-                p.next = l2;
-                l2 = l2.next;
+                l2.next = mergeTwoLists(l1, l2.next);
+                return l2;
+            }
+        }
+    }
+
+    class Solution2 {
+        public ListNode mergeTwoList(ListNode l1, ListNode l2) {
+            ListNode soldier = new ListNode(0);
+            ListNode p = soldier;
+
+            while (l1 != null && l2 != null) {
+                if (l1.val < l2.val) {
+                    p.next = l1;
+                    l1 = l1.next;
+                } else {
+                    p.next = l2;
+                    l2 = l2.next;
+                }
+                p = p.next;
             }
 
-            p = p.next;
+            if (l1 != null) p.next = l1;
+            if (l2 != null) p.next = l2;
+
+            return soldier.next;
         }
-
-        if (l1 != null) p.next = l1;
-        if (l2 != null) p.next = l2;
-
-        return soldier.next;
     }
 }
