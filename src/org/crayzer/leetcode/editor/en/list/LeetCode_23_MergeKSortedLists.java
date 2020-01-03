cea26dedffe061a0242ee4c21dc6ba6fa12d3125
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-01-03 16:08:23 +0800

    [+]  合并K个排序链表 解法提交@！

---
+++ src/org/crayzer/leetcode/editor/en/list/LeetCode_23_MergeKSortedLists.java
@@ -0,0 +1,74 @@
+package org.crayzer.leetcode.editor.en.list;
+
+public class LeetCode_23_MergeKSortedLists {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    class Solution {
+        public ListNode mergeKLists(ListNode[] lists) {
+            int len = lists.length;
+            if (len == 0) return null;
+            return mergeKLists(lists, 0, len - 1);
+        }
+
+        private ListNode mergeKLists(ListNode[] lists, int left, int right) {
+            if (left == right) return lists[left];
+            int mid = (left + right) >>> 1;
+            ListNode l1 = mergeKLists(lists, left, mid);
+            ListNode l2 = mergeKLists(lists, mid + 1, right);
+            return mergeTwoSortedLists(l1, l2);
+        }
+
+        private ListNode mergeTwoSortedLists(ListNode l1, ListNode l2) {
+            if (l1 == null) return l2;
+            if (l2 == null) return l1;
+            if (l1.val < l2.val) {
+                l1.next = mergeTwoSortedLists(l1.next, l2);
+                return l1;
+            } else {
+                l2.next = mergeTwoSortedLists(l1, l2.next);
+                return l2;
+            }
+        }
+    }
+
+    class Solution2 {
+        public ListNode mergeKLists(ListNode[] lists) {
+            int len = lists.length;
+            if (len == 0) return null;
+
+            ListNode res = lists[0];
+            for (int i = 1; i < len; i++) {
+                if (lists != null) {
+                    res = mergeTwoSortedLists(res, lists[i]);
+                }
+            }
+            return res;
+        }
+
+        private ListNode mergeTwoSortedLists(ListNode l1, ListNode l2) {
+            ListNode soldier = new ListNode(0);
+            ListNode p = soldier;
+
+            while (l1 != null && l2 != null) {
+                if (l1.val < l2.val) {
+                    p.next = l1;
+                    l1 = l1.next;
+                } else {
+                    p.next = l2;
+                    l2 = l2.next;
+                }
+                p = p.next;
+            }
+            if (l1 != null) p.next = l1;
+            if (l2 != null) p.next = l2;
+            return soldier.next;
+        }
+    }
+}
