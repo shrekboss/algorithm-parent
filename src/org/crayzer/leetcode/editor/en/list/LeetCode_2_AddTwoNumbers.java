ee3ce2394b2a7f2a1456de984f7db3dc720b4a08
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-01-07 15:53:38 +0800

    [+]  code review@!

--- src/org/crayzer/leetcode/editor/en/list/LeetCode_2_AddTwoNumbers.java
+++ src/org/crayzer/leetcode/editor/en/list/LeetCode_2_AddTwoNumbers.java
@@ -7,20 +7,18 @@ public class LeetCode_2_AddTwoNumbers {
         public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
             ListNode soldier = new ListNode(0);
             ListNode cur = soldier;
-            ListNode p = l1;
-            ListNode q = l2;
             int carray = 0;
 
-            while (p != null || q != null) {
-                int x = p != null ? p.val : 0;
-                int y = q != null ? q.val : 0;
+            while (l1 != null || l2 != null) {
+                int x = l1 != null ? l1.val : 0;
+                int y = l2 != null ? l2.val : 0;
                 int sum = carray + x + y;
                 carray = sum / 10;
                 cur.next = new ListNode(sum % 10);
                 cur = cur.next;
 
-                if (p != null) p = p.next;
-                if (q != null) q = q.next;
+                if (l1 != null) l1 = l1.next;
+                if (l2 != null) l2 = l2.next;
             }
             if (carray > 0) cur.next = new ListNode(carray);
             return soldier.next;
