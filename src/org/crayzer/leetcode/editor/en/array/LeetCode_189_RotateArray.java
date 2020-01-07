e0396fac43d29aeb8370326313db7aa9834367d5
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-01-07 15:00:23 +0800

    [+]  code review@!

--- src/org/crayzer/leetcode/editor/en/array/LeetCode_189_RotateArray.java
+++ src/org/crayzer/leetcode/editor/en/array/LeetCode_189_RotateArray.java
@@ -8,7 +8,7 @@ public class LeetCode_189_RotateArray {
 
             reverse(nums, 0, nums.length - 1);
             reverse(nums, 0, k-1);
-            reverse(nums, k+1, nums.length-1);
+            reverse(nums, k, nums.length-1);
         }
 
         private void reverse(int[] nums, int start, int end) {
