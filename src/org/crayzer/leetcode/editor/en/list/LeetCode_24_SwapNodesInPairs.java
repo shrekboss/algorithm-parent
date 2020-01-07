47b571afbefbf074dfd9c26b1a6a73940e0e61d2
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-01-07 16:26:56 +0800

    [+]  两两交换链表中的节点 解法提交@！

--- src/org/crayzer/leetcode/editor/en/list/LeetCode_24_SwapNodesInPairs.java
+++ src/org/crayzer/leetcode/editor/en/list/LeetCode_24_SwapNodesInPairs.java
@@ -4,20 +4,32 @@ import org.crayzer.leetcode.editor.en.ListNode;
 
 public class LeetCode_24_SwapNodesInPairs {
 
-    public ListNode swapPairs(ListNode head) {
-        ListNode soldier = new ListNode(0);
-        soldier.next = head;
-        ListNode tmp = soldier;
-
-        while (tmp.next != null && tmp.next.next != null) {
-            ListNode start = tmp.next;
-            ListNode end = tmp.next.next;
-            tmp.next = end;
-            start.next = end.next;
-            end.next = start;
-            tmp = start;
+    class Solution {
+        public ListNode swapPairs(ListNode head) {
+            if (head == null || head.next == null) return head;
+            ListNode next = head.next;
+            head.next = swapPairs(next.next);
+            next.next = head;
+            return next;
         }
+    }
 
-        return soldier.next;
+    class Solution2 {
+        public ListNode swapPairs(ListNode head) {
+            ListNode soldier = new ListNode(0);
+            soldier.next = head;
+            ListNode tmp = soldier;
+
+            while (tmp.next != null && tmp.next.next != null) {
+                ListNode start = tmp.next;
+                ListNode end = tmp.next.next;
+                tmp.next = end;
+                start.next = end.next;
+                end.next = start;
+                tmp = start;
+            }
+
+            return soldier.next;
+        }
     }
 }
