8cf7e6b98010b36fad645b4f1a9bc807e1f3ad3d
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-01-09 15:15:28 +0800

    [+]  code review@!

--- src/org/crayzer/leetcode/editor/en/dynamic_programming/LeetCode_714_BestTimeToBuyAndSellStockWithTransactionFee.java
+++ src/org/crayzer/leetcode/editor/en/dynamic_programming/LeetCode_714_BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -10,7 +10,7 @@ public class LeetCode_714_BestTimeToBuyAndSellStockWithTransactionFee {
             for (int i = 0; i < n; i++) {
                 int temp = dp_i_0;
                 dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
-                dp_i_1 = Math.max(dp_i_1, dp_i_0 - prices[i] - fee);
+                dp_i_1 = Math.max(dp_i_1, temp - prices[i] - fee);
             }
             return dp_i_0;
         }
