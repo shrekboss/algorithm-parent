05132769f108405e7de7fdba841e1ce68c1f6add
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-01-09 11:22:50 +0800

    [+]  买卖股票的最佳时机 解法提交@！

--- src/org/crayzer/leetcode/editor/en/dynamic_programming/LeetCode_121_BestTimeToBuyAndSellStock.java
+++ src/org/crayzer/leetcode/editor/en/dynamic_programming/LeetCode_121_BestTimeToBuyAndSellStock.java
@@ -47,4 +47,20 @@ public class LeetCode_121_BestTimeToBuyAndSellStock {
             return dp[n - 1][0];
         }
     }
+
+    class Solution1 {
+        public int maxProfit(int[] prices) {
+            int n = prices.length;
+            if (n == 0) return 0;
+            // base case: dp[-1][0] = 0, dp[-1][1] = -infinity
+            int dp_i_0 = 0, dp_i_1 = Integer.MIN_VALUE;
+            for (int i = 0; i < n; i++) {
+                // dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+                dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+                // dp[i][1] = max(dp[i-1][1], -prices[i])
+                dp_i_1 = Math.max(dp_i_1, -prices[i]);
+            }
+            return dp_i_0;
+        }
+    }
 }
