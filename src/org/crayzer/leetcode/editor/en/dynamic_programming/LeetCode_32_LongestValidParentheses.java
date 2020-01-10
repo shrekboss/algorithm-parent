18034779b3a80f7610ef5cb7f96b69ea07a248a7
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-01-10 16:17:25 +0800

    [+]  最长有效括号 解法提交@！

--- src/org/crayzer/leetcode/editor/en/dynamic_programming/LeetCode_32_LongestValidParentheses.java
+++ src/org/crayzer/leetcode/editor/en/dynamic_programming/LeetCode_32_LongestValidParentheses.java
@@ -6,6 +6,25 @@ import java.util.Stack;
 
 public class LeetCode_32_LongestValidParentheses {
 
+    class Solutionn2 {
+        public int longestValidParentheses(String s) {
+            if (s == null || s.length() == 0) return 0;
+            int maxLen = 0;
+            int[] dp = new int[s.length()];
+            for (int i = 1; i < s.length(); i++) {
+                if (s.charAt(i) == ')') {
+                    if (s.charAt(i - 1) == '(') {
+                        dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                    } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                        dp[i] = dp[i - 1] + 2 + ((i - dp[i - 1] >= 2) ? dp[i - dp[i - 1] -2] : 0);
+                    }
+                    maxLen = Math.max(maxLen, dp[i]);
+                }
+            }
+            return maxLen;
+        }
+    }
+
     /**
      * 对于这种括号匹配问题，一般都是使用栈
      * 我们先找到所有可以匹配的索引号，然后找出最长连续数列！
