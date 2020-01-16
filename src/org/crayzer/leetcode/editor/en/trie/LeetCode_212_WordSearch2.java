1d098b854bdc6c281ec0be45c2157f521de8964b
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-01-16 15:34:39 +0800

    [+]  code review@!

--- src/org/crayzer/leetcode/editor/en/trie/LeetCode_212_WordSearch2.java
+++ src/org/crayzer/leetcode/editor/en/trie/LeetCode_212_WordSearch2.java
@@ -7,49 +7,18 @@ import java.util.Set;
 
 public class LeetCode_212_WordSearch2 {
 
-    class Solution {
-        public List<String> findWords(char[][] board, String[] words) {
-            WordTrie mytrie = new WordTrie();
-            TrieNode root = mytrie.root;
-            for (String s : words) {
-                mytrie.insert(s);
-            }
+    class TrieNode {
+        public String val;
+        public boolean isLeaf;
+        public TrieNode[] links = new TrieNode[26];
 
-            Set<String> resultSet = new HashSet<>();
-            int m = board.length;
-            int n = board[0].length;
-            boolean[][] visited = new boolean[m][n];
-            for (int i = 0; i < m; i++) {
-                for (int j = 0; j < n; j++) {
-                    find(board, visited, i, j, m, n, resultSet, root);
-                }
-            }
-            System.out.println(resultSet);
-            return new LinkedList<>(resultSet);
-        }
-
-        private void find(char[][] board, boolean[][] visited, int i, int j, int m, int n, Set<String> resultSet, TrieNode cur) {
-            if (i < 0 || i >= m || j < 0 || j >= n || visited[i][j]) return;
-            cur = cur.links[board[i][j] - 'a'];
-            visited[i][j] = true;
-
-            if (cur == null) {
-                visited[i][j] = false;
-                return;
-            }
-
-            if (cur.isLeaf) resultSet.add(cur.val);
-            find(board, visited, i + 1, j, m, n, resultSet, cur);
-            find(board, visited, i, j +  1, m, n, resultSet, cur);
-            find(board, visited, i, j - 1, m, n, resultSet, cur);
-            find(board, visited, i - 1, j, m, n, resultSet, cur);
-            visited[i][j] = false;
+        public TrieNode() {
         }
     }
 
-    //字典树
     class WordTrie {
         public TrieNode root = new TrieNode();
+
 
         public void insert(String word) {
             TrieNode node = root;
@@ -62,13 +31,43 @@ public class LeetCode_212_WordSearch2 {
         }
     }
 
-    //字典树结点
-    class TrieNode {
-        public String val;
-        public boolean isLeaf;
-        public TrieNode[] links = new TrieNode[26];
+    class Solution {
+        public List<String> findWords(char[][] board, String[] words) {
+            WordTrie mytrie = new WordTrie();
+            TrieNode root = mytrie.root;
+            for (String word : words) {
+                mytrie.insert(word);
+            }
 
-        public TrieNode() {
+            Set<String> resultSet = new HashSet<>();
+            int m = board.length;
+            int n = board[0].length;
+            boolean[][] visited = new boolean[m][n];
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    find(board, visited, i, j, m, n, resultSet, root);
+                }
+            }
+            return new LinkedList<>(resultSet);
+        }
+
+        private void find(char[][] board, boolean[][] visited, int i, int j, int m, int n, Set<String> resultSet, TrieNode cur) {
+            if (i < 0 || i >= m || j < 0 || j >= n || visited[i][j]) return;
+            cur = cur.links[board[i][j] - 'a'];
+            visited[i][j] = true;
+            if (cur == null) {
+                visited[i][j] = false;
+                return;
+            }
+
+            if (cur.isLeaf) resultSet.add(cur.val);
+            find(board, visited, i + 1, j, m, n, resultSet, cur);
+            find(board, visited, i, j + 1, m, n, resultSet, cur);
+            find(board, visited, i, j - 1, m, n, resultSet, cur);
+            find(board, visited, i - 1, j, m, n, resultSet, cur);
+
+            visited[i][j] = false;
         }
     }
 }
