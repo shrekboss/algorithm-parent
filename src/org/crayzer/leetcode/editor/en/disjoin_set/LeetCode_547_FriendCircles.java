f418bfd4529fc808cce6f3e056c3a9bbd43db8ef
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-01-17 18:40:44 +0800

    [+]  code review@!

--- src/org/crayzer/leetcode/editor/en/disjoin_set/LeetCode_547_FriendCircles.java
+++ src/org/crayzer/leetcode/editor/en/disjoin_set/LeetCode_547_FriendCircles.java
@@ -38,4 +38,38 @@ public class LeetCode_547_FriendCircles {
             return ds.count();
         }
     }
+
+    class DisjoinSet {
+        private int count = 0;
+        private int[] parent;
+
+        public DisjoinSet(int n) {
+            count = n;
+            parent = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        public int find(int p) {
+            while (p != parent[p]) {
+                parent[p] = parent[parent[p]];
+                p = parent[p];
+            }
+            return p;
+        }
+
+        public void union(int q, int p) {
+            int rootP = find(p);
+            int rootQ = find(q);
+            if (rootP == rootQ) return;
+            parent[rootP] = rootQ;
+            count--;
+        }
+
+        public int count() {
+            return count;
+        }
+    }
 }
