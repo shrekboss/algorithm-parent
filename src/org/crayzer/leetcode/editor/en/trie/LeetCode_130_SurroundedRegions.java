459c4d4e3055feb4184bf424d1f65437cf9f2b74
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-01-20 18:14:42 +0800

    [+]  code review@!

--- src/org/crayzer/leetcode/editor/en/trie/LeetCode_130_SurroundedRegions.java
+++ src/org/crayzer/leetcode/editor/en/trie/LeetCode_130_SurroundedRegions.java
@@ -3,32 +3,32 @@ package org.crayzer.leetcode.editor.en.trie;
 public class LeetCode_130_SurroundedRegions {
 
     class Solution {
-        int[] unionSet; // union find set
+        int[] parent; // union find set
         boolean[] hasEdgeO; // whether an union has an 'O' which is on the edge of the matrix
 
         public void solve(char[][] board) {
             if (board.length == 0 || board[0].length == 0) return;
 
             // init, every char itself is an union
-            int height = board.length, width = board[0].length;
-            unionSet = new int[height * width];
-            hasEdgeO = new boolean[unionSet.length];
-            for (int i = 0; i < unionSet.length; i++) unionSet[i] = i;
+            int rows = board.length, cols = board[0].length;
+            parent = new int[rows * cols];
+            hasEdgeO = new boolean[parent.length];
+            for (int i = 0; i < parent.length; i++) parent[i] = i;
             for (int i = 0; i < hasEdgeO.length; i++) {
-                int x = i / width, y = i % width;
-                hasEdgeO[i] = (board[x][y] == 'O' && (x == 0 || x == height - 1 || y == 0 || y == width - 1));
+                int x = i / cols, y = i % cols;
+                hasEdgeO[i] = (board[x][y] == 'O' && (x == 0 || x == rows - 1 || y == 0 || y == cols - 1));
             }
 
             // iterate the matrix, for each char, union it + its upper char + its right char if they equals to each other
-            for (int i = 0; i < unionSet.length; i++) {
-                int x = i / width, y = i % width, up = x - 1, right = y + 1;
-                if (up >= 0 && board[x][y] == board[up][y]) union(i, i - width);
-                if (right < width && board[x][y] == board[x][right]) union(i, i + 1);
+            for (int i = 0; i < parent.length; i++) {
+                int x = i / cols, y = i % cols, up = x - 1, right = y + 1;
+                if (up >= 0 && board[x][y] == board[up][y]) union(i, i - cols);
+                if (right < cols && board[x][y] == board[x][right]) union(i, i + 1);
             }
 
             // for each char in the matrix, if it is an 'O' and its union doesn't has an 'edge O', the whole union should be setted as 'X'
-            for (int i = 0; i < unionSet.length; i++) {
-                int x = i / width, y = i % width;
+            for (int i = 0; i < parent.length; i++) {
+                int x = i / cols, y = i % cols;
                 if (board[x][y] == 'O' && !hasEdgeO[findSet(i)])
                     board[x][y] = 'X';
             }
@@ -39,14 +39,14 @@ public class LeetCode_130_SurroundedRegions {
             int rootY = findSet(y);
             // if there is an union has an 'edge O',the union after merge should be marked too
             boolean hasEdgeO = this.hasEdgeO[rootX] || this.hasEdgeO[rootY];
-            unionSet[rootX] = rootY;
+            parent[rootX] = rootY;
             this.hasEdgeO[rootY] = hasEdgeO;
         }
 
         private int findSet(int x) {
-            if (unionSet[x] == x) return x;
-            unionSet[x] = findSet(unionSet[x]);
-            return unionSet[x];
+            if (parent[x] == x) return x;
+            parent[x] = findSet(parent[x]);
+            return parent[x];
         }
     }
 
