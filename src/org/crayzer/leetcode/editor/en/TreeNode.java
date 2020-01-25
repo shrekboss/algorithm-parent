9f4df96ccec6fec2b124f0221daa257a7a012803
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-01-25 22:14:49 +0800

    [+]  二叉树的最近公共祖先 解法提交@！

---
+++ src/org/crayzer/leetcode/editor/en/TreeNode.java
@@ -0,0 +1,10 @@
+package org.crayzer.leetcode.editor.en;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
--- src/org/crayzer/leetcode/editor/en/tree/LeetCode_236_LowestCommonAncestorOfABinaryTree.java
+++ src/org/crayzer/leetcode/editor/en/tree/LeetCode_236_LowestCommonAncestorOfABinaryTree.java
@@ -1,22 +1,44 @@
 package org.crayzer.leetcode.editor.en.tree;
 
+import org.crayzer.leetcode.editor.en.TreeNode;
+
 /**
  * 二叉树的最近公共祖先
  */
 public class LeetCode_236_LowestCommonAncestorOfABinaryTree {
 
-    class Solution {
-        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-            return null;
+    class Solution1 {
+        class Solution {
+            public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+                if (root == null || root == p || root == q) return root;
+                TreeNode left = lowestCommonAncestor(root.left, p, q);
+
+                if (left != null && left != q && left != p) return left;
+                TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+                if (left != null && right != null) return root;
+
+                else
+                    return left == null ? right : left;
+            }
         }
     }
 
-    class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        public TreeNode(int val) {
-            this.val = val;
+    class Solution {
+        TreeNode ans = null;
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            recursion(root, p, q);
+            return ans;
+        }
+
+        private boolean recursion(TreeNode root, TreeNode p, TreeNode q) {
+            if (root == null) return false;
+            int left = recursion(root.left, p, q) ? 1 : 0;
+            int right = recursion(root.right, p, q) ? 1 : 0;
+            int mid = root == p || root == q ? 1 : 0;
+            if (mid + left + right >= 2) ans = root;
+            return (mid + left + right) > 0;
         }
     }
+
 }
