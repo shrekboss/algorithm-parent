612fc6b161022641fc10b9a890679b2777c0fadd
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-01-28 00:27:56 +0800

    [+]  code review@!

--- src/org/crayzer/leetcode/editor/en/dfs_bfs/LeetCode_433_MinimumGeneticMutation.java
+++ src/org/crayzer/leetcode/editor/en/dfs_bfs/LeetCode_433_MinimumGeneticMutation.java
@@ -16,9 +16,47 @@ public class LeetCode_433_MinimumGeneticMutation {
         Solution solution = new Solution();
 
         String start = "AACCGGTT";
-        String end = "AACCGGTA";
-        String[] bank = {"AACCGGTA"};
+        String end = "AAACGGTA";
+        String[] bank = {"AACCGGTA", "AACCGCTA", "AAACGGTA"};
         System.out.println(solution.minMutation(start, end, bank));
+    }
+
+    static class Solution1 {
+        public int minMutation(String start, String end, String[] bank) {
+            int res = backtrack(start, end, bank, new boolean[bank.length]);
+            if (res > bank.length) {
+                return -1;
+            }
+            return res;
+        }
+
+        private int backtrack(String current, String end, String[] bank, boolean[] used) {
+            if (current.equals(end)) {
+                return 0;
+            }
+            int min = bank.length + 1;
+            for (int i = 0; i < bank.length; i++) {
+                if (!used[i] && match(current, bank[i])) {
+                    used[i] = true;
+                    min = Math.min(1 + backtrack(bank[i], end, bank, used), min);
+                    used[i] = false;
+                }
+            }
+            return min;
+        }
+
+        private boolean match(String a, String b) {
+            int count = 0;
+            for (int i = 0; i < 8; i++) {
+                if (a.charAt(i) != b.charAt(i)) {
+                    count++;
+                }
+                if (count > 1) {
+                    return false;
+                }
+            }
+            return count == 1;
+        }
     }
 
     static class Solution {
