c74c0a388af98a64e17af143fad440138f2012c7
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-02-05 21:29:07 +0800

    [+]  初级排序 解法提交@！

--- src/org/crayzer/leetcode/editor/en/sort/primary_sort/BubbleSort.java
+++ src/org/crayzer/leetcode/editor/en/sort/primary_sort/BubbleSort.java
@@ -1,17 +1,17 @@
-package org.crayzer.leetcode.editor.en.sort;
+package org.crayzer.leetcode.editor.en.sort.primary_sort;
 
 public class BubbleSort {
 
     public static void main(String[] args) {
         int[] nums = {3, 6, 2, 4, 6, 1};
-        int[] res = bubbleSort(nums);
+        bubbleSort(nums);
 
-        for (int i = 0; i < res.length; i++) {
-            System.out.print(res[i] + " ");
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
         }
     }
 
-    public static int[] bubbleSort(int[] nums) {
+    public static void bubbleSort(int[] nums) {
         int len = nums.length;
         for (int i = 0; i < len - 1; i++) {
             for (int j = 0; j < len - i - 1; j++) {
@@ -22,6 +22,5 @@ public class BubbleSort {
                 }
             }
         }
-        return nums;
     }
 }
