b6ab7879f01ef5f882e896df3ccf07aebc9bcd35
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-02-10 23:18:09 +0800

    [+]  解码方法 解法提交@！

--- src/org/crayzer/leetcode/editor/en/dynamic_programming/LeetCode_91_DecodeWays.java
+++ src/org/crayzer/leetcode/editor/en/dynamic_programming/LeetCode_91_DecodeWays.java
@@ -3,10 +3,49 @@ package org.crayzer.leetcode.editor.en.dynamic_programming;
 import java.util.HashMap;
 
 public class LeetCode_91_DecodeWays {
+    class Solution3 {
+        public int numDecoding(String s) {
+            int len = s.length();
+            int end = 1;
+            int cur = 0;
+            if (s.charAt(len - 1) != '0') cur = 1;
+
+            for (int i = len - 2; i >= 0; i--) {
+                if (s.charAt(i) == '0') {
+                    end = cur;//end 前移
+                    cur = 0;
+                    continue;
+                }
+                int ans1 = cur;
+                int ans2 = 0;
+                int ten = (s.charAt(i) - '0') * 10;
+                int one = s.charAt(i + 1) - '0';
+                if (ten + one <= 26) ans2 = end;
+                end = cur;
+                cur = ans1 + ans2;
+            }
+            return cur;
+        }
+    }
 
     class Solution2 {
         public int numDecodings(String s) {
-            return 0;
+            int len = s.length();
+            int[] dp = new int[len + 1];
+            dp[len] = 1; //将递归法的结束条件初始化为 1
+            //最后一个数字不等于 0 就初始化为 1
+            if (s.charAt(len - 1) != '0') dp[len - 1] = 1;
+
+            for (int i = len - 2; i >= 0; i--) {
+                if (s.charAt(i) == '0') continue;
+                int ans1 = dp[i + 1];
+                int ans2 = 0;
+                int ten = (s.charAt(i) - '0') * 10;
+                int one = s.charAt(i + 1) - '0';
+                if (ten + one <= 26) ans2 = dp[i + 2];
+                dp[i] = ans1 + ans2;
+            }
+            return dp[0];
         }
     }
 
