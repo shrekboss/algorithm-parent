81d6d2e0d0a36ab0ca27e7b40d3d482b16051d45
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-02-13 22:10:06 +0800

    [+]  赛车 解法提交@！

--- src/org/crayzer/leetcode/editor/en/dynamic_programming/LeetCode_818_RaceCar.java
+++ src/org/crayzer/leetcode/editor/en/dynamic_programming/LeetCode_818_RaceCar.java
@@ -3,6 +3,29 @@ package org.crayzer.leetcode.editor.en.dynamic_programming;
 import java.util.Arrays;
 
 public class LeetCode_818_RaceCar {
+
+    class Solution1 {
+        public int racecar(int target) {
+            int[] dp = new int[target + 1];
+            for (int i = 1;i <= target; i++){
+                dp[i] = Integer.MAX_VALUE;
+                for (int forword = 1;(1 << forword) - 1 < 2 * i; forword++) {
+                    int j = (1 << forword) - 1;
+                    if(j == i)
+                        dp[i] = forword;
+                    else if (j > i)
+                        dp[i] = Math.min(dp[i], forword + 1 + dp[j - i]);
+                    else
+                        for(int back = 0; back < forword; back++) {
+                            int k = (1 << back) - 1;
+                            dp[i] =Math.min(dp[i], forword + 1 + back + 1 + dp[i - j + k]);
+                        }
+                }
+            }
+            return dp[target];
+        }
+    }
+
     class Solution {
         public int racecar(int target) {
             int[] dp = new int[target + 3];
