c6dcc32d48dbe5f33baa9fe013272cc141415cab
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-02-15 23:35:51 +0800

    [+]  code review@!

--- src/org/crayzer/leetcode/editor/en/string/LeetCode_14_LongestCommonPrefix.java
+++ src/org/crayzer/leetcode/editor/en/string/LeetCode_14_LongestCommonPrefix.java
@@ -6,7 +6,6 @@ public class LeetCode_14_LongestCommonPrefix {
             if (strs.length == 0) return "";
             String result = strs[0];
             for (int i = 1; i < strs.length; i++) {
-                //重点：如果result不是当前的子串，则删除最后一位再次比较，直到取出子串为止
                 while (strs[i].indexOf(result) != 0) {
                     result = result.substring(0, result.length() - 1);
                     if (result.equals(""))
