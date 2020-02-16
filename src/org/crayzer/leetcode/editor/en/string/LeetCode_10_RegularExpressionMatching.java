f2175cfb635852be8bd331bde247625133ee9d17
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-02-17 01:20:31 +0800

    [+] 无重复字符的最长子串 解法提交@！

---
+++ src/org/crayzer/leetcode/editor/en/string/LeetCode_10_RegularExpressionMatching.java
@@ -0,0 +1,27 @@
+package org.crayzer.leetcode.editor.en.string;
+
+public class LeetCode_10_RegularExpressionMatching {
+    class Solution {
+        public boolean isMatch(String s, String p) {
+            if (p == null || p.length() == 0) return (s == null || s.length() == 0);
+            boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+            dp[0][0] = true;
+
+            for (int j = 2; j <= p.length(); j++)
+                dp[0][j] = dp[0][j - 2] && p.charAt(j - 1) == '*';
+
+            for (int j = 1; j <= p.length(); j++) {
+                for (int i = 1; i <= s.length(); i++) {
+                    if (p.charAt(j - 1) == s.charAt(i - 1) || p.charAt(j - 1) == '.')
+                        dp[i][j] = dp[i - 1][j - 1];
+                    else if (p.charAt(j - 1) == '*') {
+                        dp[i][j] = dp[i][j - 2] || ((s.charAt(i - 1) == p.charAt(j - 2)
+                                || p.charAt(j - 2) == '.') && dp[i - 1][j]);
+                    }
+                }
+            }
+
+            return dp[s.length()][p.length()];
+        }
+    }
+}
