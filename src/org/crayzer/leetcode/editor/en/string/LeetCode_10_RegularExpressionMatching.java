2d02ab23ca1af639401fc2bb761ef414e5e5b9bd
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-02-17 01:28:42 +0800

    [+] 正则表达式匹配 解法提交@！

--- src/org/crayzer/leetcode/editor/en/string/LeetCode_10_RegularExpressionMatching.java
+++ src/org/crayzer/leetcode/editor/en/string/LeetCode_10_RegularExpressionMatching.java
@@ -10,8 +10,8 @@ public class LeetCode_10_RegularExpressionMatching {
             for (int j = 2; j <= p.length(); j++)
                 dp[0][j] = dp[0][j - 2] && p.charAt(j - 1) == '*';
 
-            for (int j = 1; j <= p.length(); j++) {
-                for (int i = 1; i <= s.length(); i++) {
+            for (int i = 1; i <= s.length(); i++) {
+                for (int j = 1; j <= p.length(); j++) {
                     if (p.charAt(j - 1) == s.charAt(i - 1) || p.charAt(j - 1) == '.')
                         dp[i][j] = dp[i - 1][j - 1];
                     else if (p.charAt(j - 1) == '*') {
