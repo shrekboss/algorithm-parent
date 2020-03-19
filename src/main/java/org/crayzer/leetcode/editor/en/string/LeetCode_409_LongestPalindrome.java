788c00db4072239d6e2fbdf6d66668b1d73e8a87
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-03-20 00:46:06 +0800

    [+] 409. 最长回文串 解法提交@！

---
+++ src/main/java/org/crayzer/leetcode/editor/en/string/LeetCode_409_LongestPalindrome.java
@@ -0,0 +1,25 @@
+package org.crayzer.leetcode.editor.en.string;
+
+public class LeetCode_409_LongestPalindrome {
+    class Solution {
+        public int longestPalindrome(String s) {
+            // 26 + 26 = 52, AXCII a-z A_Z 不是连续的
+            int[] cnt = new int[58];
+            for (char c : s.toCharArray()) {
+                cnt[c - 'A']++;
+            }
+            int ans = 0;
+            for (int x : cnt) {
+                /*
+                 * x & 1 = 1 奇数 x & 1 = 0 偶数
+                 * 字符出现数量加起来，偶数次数的直接加，奇数次数的减一后再加。
+                 */
+                ans += x - (x & 1);
+            }
+            /**
+             * 1. 通过给定的字符串s，构造出来的最长回文字符串，长度肯定小于等于s，如果小于s，那么最长回文串b的长度必然是奇数。
+             */
+            return ans < s.length() ? ans + 1 : ans;
+        }
+    }
+}
