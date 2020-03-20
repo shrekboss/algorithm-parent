451bb51e5edcbc7d92aa7bb1b34518d21d5d4364
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-03-20 17:33:15 +0800

    [+] 235. 二叉搜索树的最近公共祖先 解法提交@！

---
+++ src/main/java/org/crayzer/leetcode/editor/en/tree/LeetCode_235_LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,13 @@
+package org.crayzer.leetcode.editor.en.tree;
+
+import org.crayzer.leetcode.editor.en.TreeNode;
+
+public class LeetCode_235_LowestCommonAncestorOfABinarySearchTree {
+    class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if(root.val>p.val && root.val>q.val) return lowestCommonAncestor(root.left, p, q);
+            if(root.val<p.val && root.val<q.val) return lowestCommonAncestor(root.right,p,q);
+            return root;
+        }
+    }
+}
