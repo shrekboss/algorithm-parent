b493b205f7b6243f2bf95d1781c6355d57c274c5
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-03-25 18:21:09 +0800

    892. 三维形体的表面积

--- src/main/java/org/crayzer/leetcode/editor/en/geometry/LeetCode_892_三维形体的表面积.java
+++ src/main/java/org/crayzer/leetcode/editor/en/geometry/LeetCode_892_三维形体的表面积.java
@@ -2,6 +2,32 @@ package org.crayzer.leetcode.editor.en.geometry;
 
 public class LeetCode_892_三维形体的表面积 {
 
+    class Solution1 {
+        public int surfaceArea(int[][] grid) {
+            int[] dr = new int[]{0, 1, 0, -1}; // (0, 1)(1, 0)(0, -1)(-1, 0)
+            int[] dc = new int[]{1, 0, -1, 0};
+
+            int N = grid.length;
+            int ans = 0;
+
+            for (int r = 0; r < N; ++r)
+                for (int c = 0; c < N; ++c)
+                    if (grid[r][c] > 0) {
+                        ans += 2;
+                        for (int k = 0; k < 4; ++k) {
+                            int nr = r + dr[k];
+                            int nc = c + dc[k];
+                            int nv = 0;
+                            if (0 <= nr && nr < N && 0 <= nc && nc < N)
+                                nv = grid[nr][nc];
+
+                            ans += Math.max(grid[r][c] - nv, 0);
+                        }
+                    }
+            return ans;
+        }
+    }
+
     class Solution {
         public int surfaceArea(int[][] grid) {
             int n = grid.length, area = 0;
@@ -13,9 +39,9 @@ public class LeetCode_892_三维形体的表面积 {
                         // 一个柱体中：2个底面 + 所有的正方体都贡献了4个侧表面积
                         area += (level << 2) + 2;
                         // 减掉 i 与 i-1 相贴的两份表面积
-                        area -= i > 0? Math.min(level, grid[i - 1][j]) << 1: 0;
+                        area -= i > 0 ? Math.min(level, grid[i - 1][j]) << 1 : 0;
                         // 减掉 j 与 j-1 相贴的两份表面积
-                        area -= j > 0? Math.min(level, grid[i][j - 1]) << 1: 0;
+                        area -= j > 0 ? Math.min(level, grid[i][j - 1]) << 1 : 0;
                     }
                 }
             }
