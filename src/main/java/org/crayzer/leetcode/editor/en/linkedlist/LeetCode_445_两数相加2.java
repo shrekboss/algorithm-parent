958e6bea4966cf38769972c58daf11e68cc5f608
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-05-08 15:57:50 +0800

    445. 两数相加 II 添加新的解法@！

--- src/main/java/org/crayzer/leetcode/editor/en/linkedlist/LeetCode_445_两数相加2.java
+++ src/main/java/org/crayzer/leetcode/editor/en/linkedlist/LeetCode_445_两数相加2.java
@@ -2,7 +2,40 @@ package org.crayzer.leetcode.editor.en.linkedlist;
 
 import org.crayzer.leetcode.editor.en.ListNode;
 
+import java.util.Stack;
+
 public class LeetCode_445_两数相加2 {
+
+    class Solution1 {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            Stack<Integer> stack1 = new Stack<>();
+            Stack<Integer> stack2 = new Stack<>();
+
+            while (l1 != null) {
+                stack1.push(l1.val);
+                l1 = l1.next;
+            }
+
+            while (l2 != null) {
+                stack2.push(l2.val);
+                l2 = l2.next;
+            }
+
+            int carray = 0;
+            ListNode head = null;
+            while (!stack1.isEmpty() || !stack2.isEmpty() || carray > 0) {
+                int sum = carray;
+                sum += stack1.isEmpty() ? 0 : stack1.pop();
+                sum += stack2.isEmpty() ? 0 : stack2.pop();
+                carray = sum / 10;
+                ListNode node = new ListNode(sum % 10);
+                node.next = head;
+                head = node;
+            }
+
+            return head;
+        }
+    }
 
     class Solution {
         public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
