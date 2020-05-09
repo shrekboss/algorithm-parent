d030b10c691fde99f500ba276c6f1697c9811cd2
yizhe.chen <cyz_shrek2012@foxmail.com>
2020-05-09 10:01:00 +0800

    41. 缺失的第一个正数

---
+++ src/main/java/org/crayzer/leetcode/editor/en/binary_search/LeetCode_41_缺失的第一个正数.java
@@ -0,0 +1,36 @@
+package org.crayzer.leetcode.editor.en.binary_search;
+
+import java.util.Arrays;
+
+public class LeetCode_41_缺失的第一个正数 {
+    class Solution {
+        public int firstMissingPositive(int[] nums) {
+            int len = nums.length;
+            Arrays.sort(nums);
+
+            for (int i = 1; i <= len; i++) {
+                int res = binarySearch(nums, i);
+                if (res == -1) {
+                    return i;
+                }
+            }
+            return len + 1;
+        }
+
+        private int binarySearch(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+            while (left <= right) {
+                int mid = (left + right) >>> 1;
+                if (nums[mid] == target) {
+                    return mid;
+                } else if (nums[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
